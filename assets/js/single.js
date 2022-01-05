0df4df1d87103abca9d27900f46b019773a125fc
Preston Fassbender <fassbenderp0551@gmail.com>
2022-01-05 15:11:51 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 
 var displayWarning = function (repo) {
     // add text to warning container
@@ -64,9 +81,10 @@ var getRepoIssues = function (repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to home page
+            document.location.replace("./index.html");
         }
     });
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
